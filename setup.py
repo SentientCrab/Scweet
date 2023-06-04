a5d502d3ff92c701613115eafc47d83ba76a6d61
SentientCrab <2oclubs@gmail.com>
2023-06-03 23:34:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
   long_description_content_type="text/markdown",
   author = 'Yassine AIT JEDDI and Soufiane Bengadi',
   author_email = 'aitjeddiyassine@gmail.com',
-  url = 'https://github.com/Altimis/Scweet',
-  download_url = 'https://github.com/Altimis/Scweet/archive/v0.3.0.tar.gz',
+  url = 'https://github.com/SentientCrab/Scweet',
+  download_url = 'https://github.com/SentientCrab/Scweet/archive/refs/tags/release.tar.gz',
   keywords = ['twitter', 'scraper', 'python', "crawl", "following", "followers", "twitter-scraper", "tweets"],
   install_requires=['selenium', 'pandas', 'python-dotenv', 'chromedriver-autoinstaller', 'urllib3'],
   classifiers=[
